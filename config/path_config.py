203153389ad68c879aad9f06247a03fdf424e312
AmirGadami <aghad051@uottawa.ca>
2025-05-15 14:10:15 -0400

    the config yaml and path_config scripts are added

---
+++ config/path_config.py
@@ -0,0 +1,9 @@
+import os
+
+
+RAW_DIR = "artifact/raw"
+RAW_FILE_PATH = os.path.join(RAW_DIR,'raw.csv')
+TRAIN_PATH_CSV = os.path.join(RAW_DIR,'train.csv')
+TEST_PATH_CSV = os.path.join(RAW_DIR,'test.csv')
+
+CONFIG_PATH = 'config/config.yaml'
