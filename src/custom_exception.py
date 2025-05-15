3fc30086a6ead999e1a948cbe8ff4017df944c57
AmirGadami <aghad051@uottawa.ca>
2025-05-15 14:11:06 -0400

    custom exception is updated

--- src/custom_exception.py
+++ src/custom_exception.py
@@ -1,21 +1,18 @@
 import traceback
 import sys
 
-
-
-
-
 class CustomException(Exception):
-    def __init__(self, error_message, error_detail: sys):
+    def __init__(self, error_message, error_detail):
         super().__init__(error_message)
         self.error_message = self.get_detailed_error_message(error_message, error_detail)
 
     @staticmethod
-    def get_detailed_error_message(error_message, error_detail: sys):
-        _, _, exc_tb = error_detail.exc_info()
-        file_name = exc_tb.tb_frame.f_code.co_filename
-        line_number = exc_tb.tb_lineno
+    def get_detailed_error_message(error_message, error_detail):
+        last_trace = traceback.extract_tb(error_detail.__traceback__)[-1]
+        file_name = last_trace.filename
+        line_number = last_trace.lineno
         return f"Error: {error_message} | File: {file_name} | Line: {line_number}"
 
     def __str__(self):
         return self.error_message
+
