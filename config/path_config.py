53c3c1e5a052cc5ac5abab560debfa9322c9f086
AmirGadami <aghad051@uottawa.ca>
2025-05-16 20:46:12 -0400

    some path added

--- config/path_config.py
+++ config/path_config.py
@@ -7,3 +7,10 @@ TRAIN_PATH_CSV = os.path.join(RAW_DIR,'train.csv')
 TEST_PATH_CSV = os.path.join(RAW_DIR,'test.csv')
 
 CONFIG_PATH = 'config/config.yaml'
+
+
+### Data Processing
+
+PROCESSED_DIR = 'artifact/processed'
+PROCESSED_TRAIN_DATA_PATH = os.path.join(PROCESSED_DIR, 'processed_train.csv')
+PROCESSED_TEST_DATA_PATH = os.path.join(PROCESSED_DIR, 'processed_test.csv')
