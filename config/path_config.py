6e6abb607c125784881c676be689a2f7c5b708d1
AmirGadami <aghad051@uottawa.ca>
2025-05-18 18:22:38 -0400

    path is updated

--- config/path_config.py
+++ config/path_config.py
@@ -10,7 +10,10 @@ CONFIG_PATH = 'config/config.yaml'
 
 
 ### Data Processing
-
 PROCESSED_DIR = 'artifact/processed'
 PROCESSED_TRAIN_DATA_PATH = os.path.join(PROCESSED_DIR, 'processed_train.csv')
 PROCESSED_TEST_DATA_PATH = os.path.join(PROCESSED_DIR, 'processed_test.csv')
+
+
+## Model Training
+MODEL_OUTPUT = "artifact/models/lgbm_model_pkl"
